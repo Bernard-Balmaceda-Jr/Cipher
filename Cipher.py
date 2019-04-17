efb475173e3aa49bc73c703b3c5ffa8672813e1a
Bernard Balmaceda <bernard.balmaceda.jr@gmail.com>
2019-04-17 22:44:02 +1000

    Cryptography Base - initial draft for datagram base

--- Cipher.py
+++ Cipher.py
@@ -4,12 +4,24 @@
 class Cipher:
     def __init__(self):
         self._plain_text = 'plain_text'
-        self._keyword = 'keyword'
-        self._datagram = 'datagram'
+        # self._keyword
+        self._datagram = []
+        self._rows = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
     def print_msg(self):
         print("cipher test")
-        print(self._plain_text)
-        print(self._keyword)
-        print(self._datagram)
 
+        self._datagram = []
+        new_row = []
+        loop_count = len(self._rows)
+        while loop_count != 0:
+            for x in self._rows:
+                new_row.append(x)
+            self._datagram.append(new_row)
+            loop_count -= 1
+            new_row = []
+
+        for x in self._datagram:
+            print(x)
+
+        print('finished')
