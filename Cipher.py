2c65b18738992ebb12fd118ed4d4b43d25bcd32b
Bernard Balmaceda <bernard.balmaceda.jr@gmail.com>
2019-04-18 22:06:56 +1000

    Cryptography Base - matrix creation updated

--- Cipher.py
+++ Cipher.py
@@ -3,25 +3,27 @@
 
 class Cipher:
     def __init__(self):
-        self._plain_text = 'plain_text'
-        # self._keyword
-        self._datagram = []
-        self._rows = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        self._all_data_sets = []
 
     def print_msg(self):
-        print("cipher test")
+        data_set = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-        self._datagram = []
-        new_row = []
-        loop_count = len(self._rows)
-        while loop_count != 0:
-            for x in self._rows:
-                new_row.append(x)
-            self._datagram.append(new_row)
-            loop_count -= 1
-            new_row = []
+        #   1. add data_set to all_data_sets
+        self._all_data_sets.append(data_set[:])
 
-        for x in self._datagram:
+        #   2. create data set matrix
+        counter = 25
+        while counter != 0:
+            data_set.append(data_set.pop(data_set.index(data_set[0])))
+            # print(data_set)
+            self._all_data_sets.append(data_set[:])
+            counter -= 1
+
+        #   3. visual inspection of matrix
+        for x in self._all_data_sets:
             print(x)
 
-        print('finished')
+    def reorder_data_sets(self, args):
+        result = args.append(args.pop(args.index(args[0])))
+        return result
+
