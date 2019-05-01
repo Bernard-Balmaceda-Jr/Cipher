054d254421b37a27e6f67ef5eb1b8e09c72e5c48
Bernard Balmaceda Jr <bernard.balmaceda.jr@gmail.com>
2019-05-01 21:53:46 +1000

    Code Refactoring of program

--- ProgramStart.py
+++ ProgramStart.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python2.7
 
 from Cipher import *
+from CipherVigenere import *
 
 
 def main():
-    print('Program Starting:')
-    my_cipher = Cipher()
-    my_cipher.create_cipher()
-    my_cipher.create_keyword_dataset()
+    print('Vigenere Encrpytion Program - Initialised')
 
-    my_cipher.encode_text_msg('programming')
+    my_cipher = CipherVigenere()
+    my_cipher.cipher_matrix()
+
+    my_cipher.msg_encryption('We are Anonymous')
 
 
 if __name__ == '__main__':
